80b8950f2d50eeb78f46d84ac7ab42a0d95f00db
John Bailey <john@devmode.com>
2012-10-25 14:11:31 -0500

    Setup initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MwsWorkflow::Application.config.secret_token = '654a2c986f02aeffb5942315c51710ae6d8b0ba57a0773fdf8e5ee933554fe70421333b271fd04c5803625d0b26c49b2f32c01b454eed4b924d4a052efc0fb9c'
